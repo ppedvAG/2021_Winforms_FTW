a4f37c4dbd565b66039d1444d305e5d5bc48cc65
André <ruland@outlook.de>
2021-05-07 11:56:44 +0200

    DB Login mit Fehleranzeige

--- HalloDatenbank/HalloDatenbank/Form1.Designer.cs
+++ HalloDatenbank/HalloDatenbank/Form1.Designer.cs
@@ -29,23 +29,72 @@ namespace HalloDatenbank
         /// </summary>
         private void InitializeComponent()
         {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.LoadAllButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.LoadAllButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1353, 48);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // LoadAllButton
+            // 
+            this.LoadAllButton.AutoSize = true;
+            this.LoadAllButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.LoadAllButton.Location = new System.Drawing.Point(3, 3);
+            this.LoadAllButton.Name = "LoadAllButton";
+            this.LoadAllButton.Size = new System.Drawing.Size(131, 42);
+            this.LoadAllButton.TabIndex = 0;
+            this.LoadAllButton.Text = "Alle laden";
+            this.LoadAllButton.UseVisualStyleBackColor = true;
+            this.LoadAllButton.Click += new System.EventHandler(this.LoadAllButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1353, 695);
+            this.dataGridView1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(867, 576);
+            this.ClientSize = new System.Drawing.Size(1353, 743);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Die beste DB App des Tages v1.0 - GOLD EDITTION";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button LoadAllButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- HalloDatenbank/HalloDatenbank/Form1.cs
+++ HalloDatenbank/HalloDatenbank/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,24 @@ namespace HalloDatenbank
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void LoadAllButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=NreregORTHWND;Trusted_Connection=true");
+
+                con.Open();
+                MessageBox.Show("Verbindung hergestellt");
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fehler: {ex.Message}");
+            }
         }
     }
 }
